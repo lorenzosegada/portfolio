cedf8b1a7595bf57cb108ebd583b32e85c6d96dc
lorenzosegada <lorenzosegada@gmail.com>
2025-08-01 17:36:12 -0300

    se agrega foto y cambio de cv

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -4,13 +4,13 @@ const Contact = () => {
       <div className="p-8 bg-white dark:bg-[#161616] border-2 border-[#E0E0E0] dark:border-[#2A2A2A] rounded-lg flex flex-col items-center mt-3">
         <h2 className="text-[#3453ED] dark:text-[#5084FF] text-2xl">Contacto</h2>
         <p className="text-[#4A4A4A] dark:text-[#ffffff] text-center mt-2 font-body mb-5">
-        Siempre estoy abierto a nuevas oportunidades y colaboraciones. ¡No dudes en contactarme!
+          Siempre estoy abierto a nuevas oportunidades y colaboraciones. ¡No dudes en contactarme!
         </p>
-        <a href="#" download>
-          <button className="px-6 py-2   border border-transparent rounded-lg transition duration-300 ease-in-out bg-[#3453ED] text-[#f4f4f4]
-            hover:bg-white  hover:text-[#3453ED] hover:border-[#3453ED] hover:font-semibold hover:scale-105
-             dark:bg-[#5084FF] dark:text-[#ffffff] dark:hover:text-[#5084FF] dark:hover:bg-[#F5F5F5]">
-          Descargar CV
+        <a href="/CV_LorenzoSegadaLopez.pdf" download>
+          <button className="px-6 py-2 border border-transparent rounded-lg transition duration-300 ease-in-out bg-[#3453ED] text-[#f4f4f4]
+           hover:bg-white hover:text-[#3453ED] hover:border-[#3453ED] hover:font-semibold hover:scale-105
+            dark:bg-[#5084FF] dark:text-[#ffffff] dark:hover:text-[#5084FF] dark:hover:bg-[#F5F5F5]">
+            Descargar CV
           </button>
         </a>
       </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import { Github, Linkedin, Mail } from "lucide-react";
+import fotoPorfolio from '../assets/image/fotoPorfolio.jpeg';
 
 const Hero = () => {
   const email = "lorenzosegada@gmail.com";
@@ -23,14 +24,13 @@ const Hero = () => {
         </p>
 
         <div className="flex items-center space-x-6 mt-6">
-          <button
-            className="px-6 py-2 bg-[#3453ED] text-[#f4f4f4] border border-transparent rounded-lg
-             transition duration-300 ease-in-out
-             hover:bg-white hover:text-[#3453ED] hover:border-[#3453ED] hover:font-semibold hover:scale-105
-            dark:bg-[#5084FF] dark:text-[#ffffff] dark:hover:text-[#5084FF] dark:hover:bg-[#ffff]"
-          >
+          <a href="/CV_LorenzoSegadaLopez.pdf" download>
+          <button className="px-6 py-2 border border-transparent rounded-lg transition duration-300 ease-in-out bg-[#3453ED] text-[#f4f4f4]
+           hover:bg-white hover:text-[#3453ED] hover:border-[#3453ED] hover:font-semibold hover:scale-105
+            dark:bg-[#5084FF] dark:text-[#ffffff] dark:hover:text-[#5084FF] dark:hover:bg-[#F5F5F5]">
             Contáctame
           </button>
+        </a>
 
           <a
             href="https://github.com/lorenzosegada"
@@ -56,10 +56,14 @@ const Hero = () => {
 
       {/* Imagen o avatar */}
       <div className="w-full md:w-1/2 flex justify-center md:justify-end">
-        <div className="w-64 h-80 bg-gray-300 border-2 rounded-xl">
-          {/* Acá va la imagen o placeholder */}
-        </div>
+      <div className="w-64 h-80  rounded-xl overflow-hidden">
+        <img
+      src={fotoPorfolio}
+      alt="Foto de portfolio"
+      className="w-full h-full object-cover"
+    />
       </div>
+    </div>
     </section>
   );
 };
