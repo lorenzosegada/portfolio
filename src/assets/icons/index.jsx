bf6dfda3d72b05f8a8ac66f3d46bd3002d9284f8
lorenzosegada <lorenzosegada@gmail.com>
2025-08-02 15:17:44 -0300

    se cambia a mayuscula el icono react icon

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -9,7 +9,7 @@ import cssIcon from  '../icons/cssIcon.png'
 import cSharpIcon from '../icons/cSharpIcon.png'
 import mySQL from '../icons/mySQL.png'
 
-import reactIconBlack from '../icons/reactIconBlack.png' // se cambia el nombre del archivo react icon
+import ReactIconBlack from '../icons/ReactIconBlack.png' // se cambia el nombre del archivo react icon
 import tailwindIconBlack from '../icons/tailwindIconBlack.png'
 import nodeJsIconBlack from '../icons/nodeJsIconBlack.png'
 import javascriptIconBlack from '../icons/javascriptIconBlack.png'
@@ -22,7 +22,7 @@ import mySQLBlack from '../icons/mySQLBlack.png'
 
 
 const icons = {
-    reactIcon, tailwindIcon, nodeJsIcon, javascriptIcon, htmlIcon, gitIcon, figmaIcon, cssIcon, cSharpIcon,mySQL,reactIconBlack,tailwindIconBlack,nodeJsIconBlack,javascriptIconBlack,htmlIconBlack,gitIconBlack,figmaIconBlack,cssIconBlack,cSharpIconBlack,mySQLBlack
+    reactIcon, tailwindIcon, nodeJsIcon, javascriptIcon, htmlIcon, gitIcon, figmaIcon, cssIcon, cSharpIcon,mySQL,ReactIconBlack,tailwindIconBlack,nodeJsIconBlack,javascriptIconBlack,htmlIconBlack,gitIconBlack,figmaIconBlack,cssIconBlack,cSharpIconBlack,mySQLBlack
 };
 
 
