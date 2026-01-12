8d71869529a9174a2f5dee87bf8f49107693277c
lorenzosegada <lorenzosegada@gmail.com>
2026-01-12 13:27:32 -0300

    fix:se arregla el mail

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import fotoPorfolio from '../assets/image/fotoPorfolio.jpeg';
 const Hero = () => {
   const email = "lorenzosegada@gmail.com";
   const subject = "Contacto desde el portfolio";
-  const body = "Hola, ¿cómo puedo ayudarte?";
+  const body = "Hola Lorenzo,\n\nVi tu portfolio y me gustaría contactarte.\n\nSaludos,";
 
   const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
@@ -25,12 +25,12 @@ const Hero = () => {
 
         <div className="flex items-center space-x-6 mt-6">
           <a href="/CV_LorenzoSegadaLopez.pdf" download>
-          <button className="px-6 py-2 border border-transparent rounded-lg transition duration-300 ease-in-out bg-[#3453ED] text-[#f4f4f4]
+            <button className="px-6 py-2 border border-transparent rounded-lg transition duration-300 ease-in-out bg-[#3453ED] text-[#f4f4f4]
            hover:bg-white hover:text-[#3453ED] hover:border-[#3453ED] hover:font-semibold hover:scale-105
             dark:bg-[#5084FF] dark:text-[#ffffff] dark:hover:text-[#5084FF] dark:hover:bg-[#F5F5F5]">
-            Contáctame
-          </button>
-        </a>
+              Contáctame
+            </button>
+          </a>
 
           <a
             href="https://github.com/lorenzosegada"
@@ -56,14 +56,14 @@ const Hero = () => {
 
       {/* Imagen o avatar */}
       <div className="w-full md:w-1/2 flex justify-center md:justify-end">
-      <div className="w-64 h-80 rounded-xl overflow-hidden">
-        <img
-      src={fotoPorfolio}
-      alt="Foto de portfolio"
-      className="w-full h-full object-cover"
-    />
+        <div className="w-64 h-80 rounded-xl overflow-hidden">
+          <img
+            src={fotoPorfolio}
+            alt="Foto de portfolio"
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
-    </div>
     </section>
   );
 };
